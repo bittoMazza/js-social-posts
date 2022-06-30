10e1cdf7e073053d9c1bb83f295d482b7a2f7d29
bittoMazza <robertomazza99@gmail.com>
2022-06-30 13:42:22 +0200

    milestone 1 completed

--- main.js
+++ main.js
@@ -96,3 +96,39 @@ const posts = [
         "created": "2021-12-11"
     }
 ];
+const containerPost = document.getElementById('container')
+posts.forEach( (singlePost) => {
+    let newPost = document.createElement('div')
+    newPost.classList.add('post')
+    newPost.innerHTML = 
+    `<div class="post__header">
+        <div class="post-meta">
+            <div class="post-meta__icon">
+                <img class="profile-pic" src=${singlePost.author.image} alt="Phil Mangione">
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${singlePost.author.name}</div>
+                <div class="post-meta__time">${singlePost.created}</div>
+            </div>
+        </div>
+    </div>  
+    <div class="post__text">${singlePost.content}</div>
+    <div class="post__image">
+        <img src="${singlePost.media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="1">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${singlePost.likes}</b> persone
+            </div>
+        </div>
+    </div>
+`
+containerPost.append(newPost);
+} )
